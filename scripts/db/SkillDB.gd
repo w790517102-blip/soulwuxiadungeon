a77982dc2ffadba82311af9b9d7962595c9d9bce
w78blabla <w790517102@gmail.com>
2026-04-03 10:29:10 +0800

    Balance lianjuejian damage multiplier to 0.4

--- scripts/db/SkillDB.gd
+++ scripts/db/SkillDB.gd
@@ -12,7 +12,7 @@ const SKILLS := {
 		"target_scope": "single",
 		"target_side": "enemy",
 		"strike_count": 3,
-		"effects": [{"type": "damage", "power": 1.1}],
+		"effects": [{"type": "damage", "power": 0.4}],
 		"stat_scaling": {"str": 0.6, "agi": 0.3, "int": 0.1},
 		"available": {"mode": "all"},
 	},
